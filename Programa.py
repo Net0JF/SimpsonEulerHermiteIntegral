378c9a85259c74d044fbac9826b0904e2d0ccf3d
DevErnesto <jimenezfloresernesto@gmail.com>
2024-04-05 23:32:32 -0600

    funcion simpson compuesta

--- Programa.py
+++ Programa.py
@@ -8,6 +8,20 @@ def main():
     label = tk.Label(root, text="Menú:")
     label.pack()
     
+    button_simpson = tk.Button(root, text="Simposon compuesta", command = simpson_compuesta_window)
+    button_simpson.pack
+    
+def simpson_compuesta_window():
+    window = tk.Toplevel()
+    window.title("Simpson Compuesta")
+    
+    label_function = tk.Label(window, text="Función a integrar (f(x)): ")
+    label_function.pack()
+    entry_function = tk.Entry(window)
+    entry_function.pack()
+    
+
+    
     
 if __name__ == "__main__":
     main()
