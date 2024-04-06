223b351ce3df3650263fe02da37132ce4b59266d
DevErnesto <jimenezfloresernesto@gmail.com>
2024-04-06 08:52:44 -0600

    avance

--- Programa.py
+++ Programa.py
@@ -18,9 +18,35 @@ def simpson_compuesta_window():
     label_function = tk.Label(window, text="Función a integrar (f(x)): ")
     label_function.pack()
     entry_function = tk.Entry(window)
-    entry_function.pack()
-    
+    entry_function.pack()   
 
+    button_calcular= tk.Button(window, text="Calcular", comand=lambda: simpson_compuesta(entry_function.get(), entry_a.get(), entry_b.get, entry_n.get))
+    button_calcular.pack
+    
+def simpson_compuesta(a, b, n):
+    try:
+        a = float(a)
+        b = float(b)
+        n = float(n)
+        h = (b-a)/n
+        resultado=0
+        
+        for i in range (n+1):
+            xi = a + i * h
+            ki = funcion_evaluar(funcion_String, xi)
+            
+            if i == 0 or i == n:
+                resultado +=  ki
+            elif i%2==0:
+                resultado += 2*ki
+            else:
+                resultado += 4*ki
+        resultado *= h/ 3.0
+        
+        mesaggebox
+        
+    except ValueError:
+        messageBox.
     
     
 if __name__ == "__main__":
