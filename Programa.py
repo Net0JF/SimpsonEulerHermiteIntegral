f2554f80cb26301159695854525cedece1f0806e
DevErnesto <jimenezfloresernesto@gmail.com>
2024-04-05 23:20:55 -0600

    actualizacion

---
+++ Programa.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+
+def main():
+    root = tk.Tk()
+    root.title("Calculadora matemática")
+    
+    label = tk.Label(root, text="Menú:")
+    label.pack()
+    
+    
+if __name__ == "__main__":
+    main()
